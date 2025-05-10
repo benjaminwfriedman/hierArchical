98c17b07d4177e7d72d47ee23c7a3f47daea83ee
Benjamin Friedman <benjaminfriedman@macbookpro.mynetworksettings.com>
2025-05-09 22:28:18 -0400

    updated all the BaseItem transformation methods to recursivly move all sub items. Now when you move a super item all subs will move with it

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ def generate_id(prefix):
 def random_color(seed=None):
     if seed is not None:
         random.seed(seed)
-    r = random.randint(50, 200)
-    g = random.randint(50, 200)
-    b = random.randint(50, 200)
+    r = random.randint(0, 250)
+    g = random.randint(0, 250)
+    b = random.randint(0, 250)
     return f"rgb({r},{g},{b})"
